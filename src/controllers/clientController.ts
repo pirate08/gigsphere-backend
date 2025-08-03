cdabfa3a2d95ae222d5d5a6608c2441fb3adbc02
Mayukh <mayukhdebgoswami82@gmail.com>
2025-08-03 18:52:36 +0530

    Job added backend has been added..

--- src/controllers/clientController.ts
+++ src/controllers/clientController.ts
@@ -1,19 +1,20 @@
 import JobModel from '../models/job.model';
 import { Request, Response } from 'express';
 
+// --Get all jobs--
 export const getMyJobs = async (req: Request, res: Response) => {
   try {
     //  --Fetch clientId from the request (assumed set by auth middleware)--
     const clientId = req.user?.id;
 
     if (!clientId) {
-      res.status(401).json({ message: 'Unauthorized' });
+      return res.status(401).json({ message: 'Unauthorized' });
     }
 
     const jobs = await JobModel.find({ clientId });
 
     if (jobs.length === 0) {
-      res.status(200).json({
+      return res.status(200).json({
         message: 'No jobs added yet',
         jobs: [],
       });
@@ -25,6 +26,32 @@ export const getMyJobs = async (req: Request, res: Response) => {
     });
   } catch (error) {
     console.log('Error fetching client jobs', error);
-    res.status(500).json({ message: 'Server Error' });
+    return res.status(500).json({ message: 'Server Error' });
+  }
+};
+
+// -- Create a job--
+export const createJob = async (req: Request, res: Response) => {
+  try {
+    const clientId = req.user?.id;
+
+    if (!clientId) {
+      return res.status(401).json({ message: 'Unauthorized' });
+    }
+
+    const newJob = new JobModel({
+      ...req.body,
+      clientId,
+    });
+
+    const savedData = await newJob.save();
+
+    return res.status(201).json({
+      message: 'Job created Successfully',
+      job: savedData,
+    });
+  } catch (error) {
+    console.log('Failed to create job', error);
+    return res.status(500).json({ message: 'Server Error' });
   }
 };
--- src/routes/client.routes.ts
+++ src/routes/client.routes.ts
@@ -1,22 +1,14 @@
-import express, { Request, Response } from 'express';
+import express from 'express';
 import { verifyToken } from '../middlewares/auth.middleware';
 import { allowRoles } from '../middlewares/roleMiddleware';
-import { getMyJobs } from '../controllers/clientController';
+import { createJob, getMyJobs } from '../controllers/clientController';
 
 const clientRoutes = express.Router();
 
-clientRoutes.post(
-  '/jobs',
-  verifyToken,
-  allowRoles('client'),
-  (req: Request, res: Response) => {
-    return res.json({
-      message: 'Job created successfully by client!',
-      user: req.user,
-    });
-  }
-);
-
+// --Get all jobs--
 clientRoutes.get('/my-jobs', verifyToken, allowRoles('client'), getMyJobs);
+
+// --Create new Job--
+clientRoutes.post('/jobs', verifyToken, allowRoles('client'), createJob);
 
 export default clientRoutes;
