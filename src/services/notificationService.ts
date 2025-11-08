f97cf4b92a8c0ff47817c977c1cde14bb607ac30
Mayukh <mayukhdebgoswami82@gmail.com>
2025-11-09 00:05:09 +0530

    Some changes made in notification service file...

--- src/services/notificationService.ts
+++ src/services/notificationService.ts
@@ -25,7 +25,7 @@ export const notifyFreelancersOfNewJob = async (
       recipientId: freelancer._id,
       type: 'NEW_JOB_OPEN',
       message: `New job posted ${jobTitle} is now open for applications!`,
-      link: `/freelancer/jobs/${jobId}`,
+      link: `/find-work/job-details/${jobId}`,
       read: false,
     }));
 
